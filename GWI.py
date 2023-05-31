7acce2decd528029dcadc01c18c7ac9d95c4477a
Jonas van der Ham <jonasvdham@gmail.com>
2023-05-31 17:20:12 +0200

    fixed mistake in Guest e.a. of integral endpoint (t instead of tau). TODO: fix edge case from f1 to f2 in ftotal

--- GWI.py
+++ GWI.py
@@ -1,12 +1,12 @@
 import numpy as np
-from scipy.integrate import quad, cumtrapz
+from scipy.integrate import quad
 from scipy.stats import norm
 import pandas as pd
 import matplotlib.pyplot as plt
 
 
 class GWI:
-    def __init__(self, tf=200):
+    def __init__(self, tf=100, lifetime=50, rotation_period=10):
         """
         aCH4 - instant. radiative forcing per unit mass [10^-12 W/m2 /kgCH4]
         tauCH4 - lifetime (years)
@@ -17,26 +17,30 @@ class GWI:
         tf - TimeFrame in years
         time - Time Vector with length = tf
         """
+
+        # GHG constants from Hoxha et al. 2020
         self.constants = {
             "GHGs": {
                 "CO2": {
-                    "a": 0.0018088,
+                    "a": 1.76e-15,
                     "tau": [172.9, 18.51, 1.186],
                     "decay_func": self.C_CO2,
                 },
-                "CH4": {"a": 0.129957, "tau": 12, "decay_func": self.C_CH4},
+                "CH4": {"a": 1.28e-13, "tau": 12, "decay_func": self.C_CH4},
+                "N2O": {"a": 3.9e-13, "tau": 114, "decay_func": self.C_CH4},
             },
             "aBern": [0.217, 0.259, 0.338, 0.186],
         }
         self.tf = tf
-        self.time = np.arange(self.tf)
-        self.lifetime = 75
+        self.lifetime = lifetime
+        self.rotation_period = rotation_period
 
         # tmp
         self.data = {"CO2": np.zeros(self.tf), "CH4": np.zeros(self.tf)}
         self.data["CO2"][0] = 1
         self.data["CH4"][0] = 1
         self.df = pd.DataFrame(self.data)
+        self.tmp = 0
 
     def read_infile(self, skiprows=range(1, 3), sep=";"):
         self.df = pd.read_csv(self.infile, skiprows=skiprows, sep=sep)
@@ -77,50 +81,79 @@ class GWI:
     def GWI_cum(self, t):
         return sum([self.GWI_inst(i) for i in range(t)])
 
-    def plot(self, GHG):
+    def plot(self, lifetime, rotation_period):
         x = np.arange(self.tf)
-        y = self.constants[GHG]["decay_func"](x)
+        y = [
+            self.f_total(i, lifetime, rotation_period) for i in range(self.tf)
+        ]
         plt.plot(x, y)
         plt.show()
 
-    def GWPbio(self, c0):
-        # c0 - The emission pulse (kg CO2)
+    def GWPbio(self, c0=1, lifetime=None, rotation_period=None):
+        if lifetime == None:
+            lifetime = self.lifetime
+        if rotation_period == None:
+            rotation_period = self.rotation_period
+
         return c0 * (
             quad(
-                lambda x: self.constants["GHGs"]["CO2"]["a"] * self.f_total(x),
+                lambda x: self.constants["GHGs"]["CO2"]["a"]
+                * self.f_total(x, lifetime, rotation_period),
                 0,
                 self.tf,
-            )
+            )[0]
             / quad(
                 lambda x: self.constants["GHGs"]["CO2"]["a"] * self.C_CO2(x),
                 0,
                 self.tf,
-            )
+            )[0]
         )
 
-    def f_total(self, t):
-        if t < self.lifetime:
+    def f_total(self, t, lifetime, rotation_period):
+        if t < lifetime:
             return -(
                 quad(
-                    lambda x: self.growth(x) * self.C_CO2(t - x),
+                    lambda x: self.growth(x, rotation_period)
+                    * self.C_CO2(t - x),
                     0,
-                    self.lifetime,
-                )
+                    t,
+                )[0]
             )
+        elif t == lifetime:
+            self.tmp = -(
+                quad(
+                    lambda x: self.growth(x, rotation_period)
+                    * self.C_CO2(t - x),
+                    0,
+                    t,
+                )[0]
+            )
+            print(self.tmp)
+            return self.tmp
         else:
-            return self.C_CO2(t - self.lifetime) - quad(
-                lambda x: self.growth(x) * self.C_CO2(t - x), self.lifetime, t
+            return (
+                self.C_CO2(t - lifetime)
+                - quad(
+                    lambda x: self.growth(x, rotation_period)
+                    * self.C_CO2(t - x),
+                    lifetime,
+                    t,
+                )[0]
             )
 
-    def growth(self, t):
-        return self.normal_dist(t)
+    def growth(self, t, rotation_period):
+        return norm.pdf(t, loc=rotation_period / 2, scale=rotation_period / 4)
 
-    def normal_dist(x, mean=2, sd=1):
-        prob_density = (np.pi * sd) * np.exp(-0.5 * ((x - mean) / sd) ** 2)
-        return prob_density
+    def AGWP(self, GHG, t):
+        return quad(
+            lambda x: self.constants["GHGs"][GHG]["a"]
+            * self.constants["GHGs"][GHG]["decay_func"](x),
+            0,
+            t,
+        )[0]
 
 
-GWI = GWI()
+GWI = GWI(tf=500)
 
 """
     def c_CO2_vector(self, t):
